871f75eb64e0ce253743b2eeacf679db0afb4e06
Parth Ingle <parthingle@g.ucla.edu>
2019-06-01 13:12:11 -0700

    review resolution

--- src/bin/completion.js
+++ src/bin/completion.js
@@ -1,15 +1,6 @@
 // Computation of numbers and ratios in various screens
 
 // Imports
-<<<<<<< HEAD
-=======
-
-// Accepts the lengths of total and completed items
-// Returns the percentage done
-const computeCompletion = (lenBucketed, lenTotal) => {
-    return lenBucketed / lenTotal;
-};
->>>>>>> 1abf22c222d3a214c0a7ec47db48495cfb1398c9
 
 // Accepts an array: [{itemId, timestamp}]
 // Returns two arrays [itemId], [timestamp]
@@ -41,19 +32,9 @@ const isBucketed = (bucketedItems, allItems) => {
 export const prepHome = async (user, allItems) => {
     let times = isBucketed(user.myBucketedItems, allItems);
     let stampedItems = [];
-<<<<<<< HEAD
     allItems.map((item, idx) => {
         item["done"] = times[idx];
         stampedItems.push(item);
-=======
-    allItems.map(async (item, idx) => {
-        try {
-            item["done"] = times[idx];
-            stampedItems.push(item);
-        } catch (error) {
-            return Promise.reject(error);
-        }
->>>>>>> 1abf22c222d3a214c0a7ec47db48495cfb1398c9
     });
     let returnObject = {
         completionPercentage: user.myBucketedItems.length / allItems.length,
--- src/db/itemFunctions.js
+++ src/db/itemFunctions.js
@@ -59,18 +59,6 @@ export const createNewItem = async item => {
 };
 
 export const getAll = async () => {
-<<<<<<< HEAD
     const allItems = await ITEMS.get().docs.map(doc => doc.data());
     return allItems;
-=======
-    let allItems = [],
-        allItemsRef;
-    try {
-        allItemsRef = await ITEMS.get();
-        allItemsRef.docs.map(doc => allItems.push(doc.data()));
-    } catch (error) {
-        return Promise.reject(error);
-    }
-    return Promise.resolve(allItems);
->>>>>>> 1abf22c222d3a214c0a7ec47db48495cfb1398c9
 };
